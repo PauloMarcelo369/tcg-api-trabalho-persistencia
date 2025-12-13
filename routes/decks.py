b92002af180d4de44fa098a39573aeacfcd7f687
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-13 08:00:31 -0300

    adicionando consulltas personalizadas para deck

--- routes/decks.py
+++ routes/decks.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, HTTPException, Depends, status
-from sqlmodel import Session, select
+from fastapi import APIRouter, HTTPException, Depends, status, Query
+from sqlmodel import Session, select, func
 from database import get_session
+from sqlalchemy.orm import selectinload
 from models.models import Deck, DeckCardLink, Card, DeckFormat
-from routes.schemas.deckShema import DeckCreate, DeckRead, DeckUpdate
+from routes.schemas.deckShema import DeckCreate, DeckRead, DeckUpdate, DeckWithCardsRead
+from pydantic import ValidationError
+
 
 router = APIRouter(prefix="/decks", tags=["Decks"])
-
-from pydantic import ValidationError
 
 @router.post("/", response_model=DeckRead, status_code=status.HTTP_201_CREATED)
 def create_deck(data: DeckCreate, session: Session = Depends(get_session)):
@@ -82,3 +83,57 @@ def put(deck_id : int, updated_deck : DeckUpdate,  session: Session = Depends(ge
     except Exception as e:
         session.rollback()
         raise HTTPException(400, f"Não foi posssível atualizar Deck com ID {deck_id}!")
+    
+
+@router.get("/", response_model=list[DeckRead], status_code=status.HTTP_200_OK)
+def list_decks( 
+    skip: int = 0,
+    limit: int = 10,
+    session: Session = Depends(get_session),
+):
+    decks = session.exec(
+        select(Deck).offset(skip).limit(limit)
+    ).all()
+
+    return decks
+
+
+@router.get("/search/{name}",response_model=list[DeckRead])
+def search_deck_by_name(
+    name : str,
+    skip: int = 0,
+    limit: int = Query(10, le=50),
+    session: Session = Depends(get_session)
+):
+    
+    decks = session.exec(
+        select(Deck)
+        .where(Deck.name.ilike(f"%{name}%"))
+        .offset(skip)
+        .limit(limit)
+    ).all()
+
+    return decks
+
+
+@router.get("/{deck_id}/with-cards", response_model=DeckWithCardsRead, status_code=status.HTTP_200_OK)
+def get_deck_with_cards(deck_id : int, session: Session = Depends(get_session)):
+    deck = session.exec(
+        select(Deck)
+        .where(Deck.id == deck_id)
+        .options(selectinload(Deck.cards))
+    ).first()
+
+    if (not deck):
+        raise HTTPException(404, "Deck não encontrado")
+
+    return deck
+
+@router.get("/{deck_id}/cards/count")
+def count_cards_in_deck(deck_id : int,session : Session = Depends(get_session)):
+    count = session.exec(
+        select(func.sum(DeckCardLink.qty))
+        .where(DeckCardLink.deck_id == deck_id)
+    ).one()
+
+    return {"deck_id" : deck_id, "total_cards" : count or 0}
